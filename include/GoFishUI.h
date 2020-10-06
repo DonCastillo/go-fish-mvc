7e0cb040a5d57690d0e6ebf09178b91bbcb65b06
Don Castillo <don.castillo@uleth.ca>
2020-10-06 01:18:56 -0600

    added print funcs in GoFishUI

--- include/GoFishUI.h
+++ include/GoFishUI.h
@@ -1,12 +1,22 @@
 #ifndef GOFISHUI_H_INCLUDED
 #define GOFISHUI_H_INCLUDED
+#include "Player.h"
+#include "Deck.h"
+#include "Card.h"
+#include <vector>
 
 class GoFishUI {
  public:
     GoFishUI();
     ~GoFishUI();
+    void printScores(std::vector<Player*> players);
+    void printPlayerTurn(Player* pPlayer);
+    void congratulate(Player* pPlayer);
+    void printDeck(Deck* pDeck);
+    void printPlayerHand(Player* pPlayer);
 
  private:
+    void printCards(std::vector<Card*> pCards);
 };
 
 #endif // GOFISHUI_H_INCLUDED
--- src/GoFishUI.cpp
+++ src/GoFishUI.cpp
@@ -1,9 +1,41 @@
 #include "GoFishUI.h"
 #include "Card.h"
+#include "Player.h"
+#include "Deck.h"
 #include <vector>
+#include <iostream>
 
 
 GoFishUI::GoFishUI() {}
 
 GoFishUI::~GoFishUI() {}
 
+void GoFishUI::printCards(std::vector<Card*> pCards) {
+    for (Card* c: pCards) {
+        std::cout << c->getSuit() << "..." << c->getRank() << std::endl;
+    }
+}
+
+void GoFishUI::printScores(std::vector<Player*> players) {
+    for (Player* p : players) {
+        std::cout << p->getName() << std::endl;
+        std::cout << p->getScore() << std::endl;
+    }
+}
+
+void GoFishUI::printPlayerTurn(Player* pPlayer) {
+    std::cout << "It is your turn, " << pPlayer->getName() << "." << std::endl;
+}
+
+void GoFishUI::congratulate(Player* pPlayer) {
+    std::cout << "Congratulations! " << pPlayer->getName() << " wins ." << std::endl;
+}
+
+void GoFishUI::printDeck(Deck* pDeck) {
+    printCards(pDeck->getDeck());
+}
+
+void GoFishUI::printPlayerHand(Player* pPlayer) {
+    std::cout << pPlayer->getName() << "'s cards at hand:" << std::endl;
+    printCards(pPlayer->getCardHand());
+}
