afe1294f584c3e328e8e0946b9c85308af2f9b55
Don Castillo <don.castillo@uleth.ca>
2020-10-11 02:39:56 -0600

    tested getWinner

--- test/TestGoFish.cpp
+++ test/TestGoFish.cpp
@@ -11,6 +11,7 @@
 using ::testing::_;
 using ::testing::Return;
 using ::testing::Expectation;
+using ::testing::AtLeast;
 
 TEST(TestGoFish, addingAndGettingPlayers) {
     GoFish* gf = new GoFish(new GoFishUITesting());
@@ -25,17 +26,40 @@ TEST(TestGoFish, addingAndGettingPlayers) {
 
     // check non empty player vector
     EXPECT_EQ(gf->getPlayers().size(), 2);
-
     delete gf;
 }
+
 
 TEST(TestGoFish, gettingDeck) {
     GoFish* gf = new GoFish(new GoFishUITesting());
 
     // should have an uninitialized deck
     EXPECT_EQ(gf->getDeck(), nullptr);
-
-//    // after initializing the deck
-//    gf->startGame();
-//    EXPECT_NE(gf->getDeck(), nullptr);
+    delete gf;
 }
+
+
+TEST(TestGoFish, getWinner) {
+    GoFish* gf = new GoFish(new GoFishUITesting());
+
+    // no player
+    EXPECT_TRUE(gf->getWinner().empty());
+
+    // one player
+    MockPlayer michael(0, "Michael");
+    michael->updateScore(1);
+
+    EXPECT_CALL(michael, getScore())
+    .Times(AtLeast(2))
+    .WillOnce(Return(1))
+    .WillRepeatedly(Return(1));
+
+    for (Player* p : gf->getWinner()) {
+        EXPECT_EQ(p->getName(), "Michael");
+    }
+
+
+    delete gf;
+}
+
+
