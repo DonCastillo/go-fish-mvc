dc649d1b92caca6ae1465e9e04f4bf30ab631a93
Don Castillo <don.castillo@uleth.ca>
2020-10-11 00:45:38 -0600

    removed player pointers

--- test/TestGoFish.cpp
+++ test/TestGoFish.cpp
@@ -13,19 +13,15 @@ using ::testing::Expectation;
 
 TEST(TestGoFish, addingAndGettingPlayers) {
     GoFish* gf = new GoFish(new GoFishUITesting());
-    Player* jane = new Player(0, "Jane");
-    Player* don = new Player(1, "Don");
 
     // check for empty player in player's vector
     EXPECT_EQ(gf->getPlayers().size(), 0);
 
-    gf->addPlayer(jane);
-    gf->addPlayer(don);
+    gf->addPlayer(new Player(0, "Jane"););
+    gf->addPlayer(new Player(1, "Don"););
 
     // check non empty player vector
     EXPECT_EQ(gf->getPlayers().size(), 2);
 
-    delete don;
-    delete jane;
     delete gf;
 }
