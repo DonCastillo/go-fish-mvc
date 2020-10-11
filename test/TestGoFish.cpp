4c593fb7bb3de5ccf512100547dbe45daf8d7cd1
Don Castillo <don.castillo@uleth.ca>
2020-10-11 00:48:19 -0600

    removed all mock objects

--- test/TestGoFish.cpp
+++ test/TestGoFish.cpp
@@ -1,27 +1,31 @@
 #include <vector>
 #include "gtest/gtest.h"
-#include "MockGoFish.h"
-#include "MockDeck.h"
-#include "MockPlayer.h"
+//#include "MockGoFish.h"
+//#include "MockDeck.h"
+//#include "MockPlayer.h"
 #include "GoFishUITesting.h"
 #include "GoFish.h"
 #include "Player.h"
 
-using ::testing::_;
-using ::testing::Return;
-using ::testing::Expectation;
+//using ::testing::_;
+//using ::testing::Return;
+//using ::testing::Expectation;
 
 TEST(TestGoFish, addingAndGettingPlayers) {
     GoFish* gf = new GoFish(new GoFishUITesting());
+    Player* jane = new Player(0, "Jane");
+    Player* don = new Player(1, "Don");
 
     // check for empty player in player's vector
     EXPECT_EQ(gf->getPlayers().size(), 0);
 
-    gf->addPlayer(new Player(0, "Jane"));
-    gf->addPlayer(new Player(1, "Don"));
+    gf->addPlayer(jane);
+    gf->addPlayer(don);
 
     // check non empty player vector
     EXPECT_EQ(gf->getPlayers().size(), 2);
 
+    delete don;
+    delete jane;
     delete gf;
 }
