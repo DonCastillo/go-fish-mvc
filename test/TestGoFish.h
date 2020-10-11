11d8be178959a146dd44df4504618985dec0bac9
Don Castillo <don.castillo@uleth.ca>
2020-10-10 19:01:39 -0600

    added mock methods

--- test/TestGoFish.h
+++ test/TestGoFish.h
@@ -1,8 +1,10 @@
 #ifndef TESTGOFISH_H_INCLUDED
 #define TESTGOFISH_H_INCLUDED
 
+#include <vector>
 #include "gmock/gmock.h"
 #include "GoFish.h"
+
 
 using ::testing;
 
@@ -10,6 +12,18 @@ class MockGoFish : public GoFish {
  public:
     explicit MockGoFish() {}
     virtual ~MockGoFish() {}
+
+    MOCK_METHOD1(addPlayer, void(Player* pPlayer));
+    MOCK_METHOD0(deal, void());
+    MOCK_METHOD1(fish, void(Player* pPlayer));
+    MOCK_METHOD1(isThereABook, bool(Player* pPlayer));
+    MOCK_METHOD3(askCard, bool(Player* p1, Player* p2, Card* targetCard));
+    MOCK_METHOD0(getDeck, Deck*());
+    MOCK_METHOD0(getPlayers, std::vector<Player*>());
+    MOCK_METHOD0(getWinner, std::vector<Player*>());
+    MOCK_METHOD0(startGame, void());
+    MOCK_METHOD0(anyoneHasCard, bool());
+    MOCK_METHOD0(getRandomPlayer, Player*());
 }
 
 #endif // TESTGOFISH_H_INCLUDED
