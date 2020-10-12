6be1a77aed2c2e3ae7efe4d646793acb65c82d54
Don Castillo <don.castillo@uleth.ca>
2020-10-12 16:52:50 -0600

    tested selectPlayer

--- test/TestGoFishUITesting.cpp
+++ test/TestGoFishUITesting.cpp
@@ -43,3 +43,31 @@ TEST(TestRandomize, selectCardFromHand) {
     delete dummy;
     delete ui;
 }
+
+TEST(TestRandomize, selectPlayer) {
+    GoFishUITesting* ui = new GoFishUITesting();
+    Player* dummy1 = new Player(0, "Dummy1");
+    Player* dummy2 = new Player(1, "Dummy2");
+    Player* dummy3 = new Player(2, "Dummy3");
+    std::vector<Player*> allPlayers;
+    Player* selectedPlayer;
+
+    // no player to choose
+    selectedPlayer = ui->selectPlayer(dummy1, allPlayers);
+    EXPECT_EQ(selectedPlayer, nullptr);
+
+    // there is a player to choose
+    allPlayers.push_back(dummy2);
+    allPlayers.push_back(dummy3);
+    selectedPlayer = ui->selectPlayer(dummy1, allPlayers);
+    EXPECT_TRUE(selectedPlayer->getID() == 1 ||
+                selectedPlayer->getID() == 2);
+
+    EXPECT_TRUE(selectedPlayer->getName() == "Dummy2" ||
+                selectedPlayer->getName() == "Dummy3");
+
+    delete dummy1;
+    delete dummy2;
+    delete dummy3;
+    delete ui;
+}
